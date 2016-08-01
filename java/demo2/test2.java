5fde9eacaa25520073f089f3d4efbed046412d5b
wangqin <wangqin@DESKTOP-PJVERKF>
2016-08-01 12:13:58 +0800

    bean

---
+++ java/demo2/test2.java
@@ -0,0 +1,18 @@
+package demo2;
+
+import java.io.Serializable;
+
+public class test2 implements Serializable{
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+
+}
